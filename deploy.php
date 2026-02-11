acddcd2f333b7d20d4a2aaef813b4b90f6c0d4c5
Developer <developer@localhost.local>
2026-02-11 16:08:57 +0100

    Clean up deploy.php

--- deploy.php
+++ deploy.php
@@ -20,49 +20,27 @@ $productionStage->addServer('hntestjvisser1.hypernode.io');
 task('build:node' , function () {
     within("{{release_or_current_path}}/app/design/frontend/Poespas/hyva-child-one/web/tailwind", function () {
         run('npm ci');
-        run('npx hyva-sources');
         run('npm run build');
     });
     within("{{release_or_current_path}}/app/design/frontend/Poespas/hyva-child-two/web/tailwind", function () {
         run('npm ci');
-        run('npx hyva-sources');
         run('npm run build');
     });
     within("{{release_or_current_path}}/app/design/frontend/Poespas/hyva-child-three/web/tailwind", function () {
         run('npm ci');
-        run('npx hyva-sources');
         run('npm run build');
     });
     within("{{release_or_current_path}}/app/design/frontend/Poespas/hyva-child-four/web/tailwind", function () {
         run('npm ci');
-        run('npx hyva-sources');
         run('npm run build');
     });
     within("{{release_or_current_path}}/app/design/frontend/Poespas/hyva-child-five/web/tailwind", function () {
         run('npm ci');
-        run('npx hyva-sources');
         run('npm run build');
     });
 });
 
 before('magento:deploy:assets', 'build:node');
-/*
-task('magento:deploy:assets', function () {
-    invoke('magento:deploy:assets:adminhtml');
-    within("{{release_or_current_path}}", function () {
-        run('wget https://github.com/elgentos/magento2-static-deploy/releases/download/0.0.2/magento2-static-deploy-linux-amd64 -O /tmp/magento2-static-deploy');
-        run('chmod +x /tmp/magento2-static-deploy');
-    });
-
-    $themes = get('magento_themes');
-    foreach ($themes as $theme => $locales) {
-        within("{{release_or_current_path}}", function () use ($theme, $locales) {
-            run('echo "Deploying static content for theme ' . $theme . ' and locales: ' . $locales . '"');
-            run('/tmp/magento2-static-deploy -root . -themes ' . $theme . ' -locales ' . join(',', explode(' ', $locales)) . ' -areas frontend -v -content-version {{ content_version }}');
-        });
-    }
-});
-*/
 
 $configuration->enableHighPerformanceStaticDeploy();
 
